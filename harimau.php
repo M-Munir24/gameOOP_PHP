ec1b49eb6ce2a4d2f362cc8817f2b99cfdb0f930
m-munir24 <52531952+M-Munir24@users.noreply.github.com>
2021-02-16 06:02:17 +0700

    Update harimau.php

--- harimau.php
+++ harimau.php
@@ -22,15 +22,11 @@ if ($hewan1->get_nama() == 'harimau') {
 	$fight1->attackPower(7);
 	$fight1->defencePower(8);
 
-
-
-	 $demeg = $hewan1->get_darah() - $fight1->get_attackPower() / $fight1->get_defencePower();
-
     echo "Nama = ".$hewan1->get_nama()."<br>";
 
     echo "jumlah Kaki = ".$hewan1->get_jumlahKaki()."<br>";
 
-    echo "Darah = ".$demeg."<br>";
+    echo "Darah = ".$hewan1->get_darah()."<br>";
     echo "Defence Power = ".$fight1->get_defencePower()."<br>";
     echo "Attack Power = ".$fight1->get_attackPower()."<br>";
 
